93fe7fb7c83d208b3430ce4b93fe742c4914572d
Leight Johnson <leightjohnson93@gmail.com>
2019-03-25 22:24:04 -0600

    added weather forecast summary and todays high/low/weather

--- src/components/Resort.tsx
+++ src/components/Resort.tsx
@@ -34,10 +34,13 @@ const Resort: React.FC<{
     weatherForecast,
   } = forecast
   const { name, logoURLString } = mountain
-  const todayForecast = weatherForecast.find(day => day.daycode === 0)
-  const { dayDescription, forecastString, temperatureHigh } = todayForecast
-    ? todayForecast
-    : { dayDescription: '', forecastString: '', temperatureHigh: '' }
+
+  const getDayForecast = (dayInt: number) => {
+    const todayForecast = weatherForecast.find(day => day.daycode === dayInt)
+    return todayForecast
+      ? todayForecast
+      : { dayDescription: '', forecastString: '', temperatureHigh: '' }
+  }
 
   const snowfallToHex = (): string =>
     Math.floor((weightedSnowfall / maxWeightedSnowfall) * 255).toString(16)
@@ -45,6 +48,7 @@ const Resort: React.FC<{
   const styles = createStyles({
     card: {
       backgroundColor: `${theme.palette.primary.light}${snowfallToHex()}`,
+      height: expanded ? 'auto' : 400,
     },
     gridItem: {
       padding: useMediaQuery('(max-width:600px)') ? '8px 0px' : '8px 8px',
@@ -52,44 +56,70 @@ const Resort: React.FC<{
     cardHeader: {
       textAlign: 'center',
     },
-    cardActionArea: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    media: {
-      height: 150,
-      width: 150,
-      marginLeft: -50,
-      marginTop: -40,
+    weatherToday: {
+      maxWidth: 225,
     },
     expand: {
       transform: expanded ? `rotate(180deg)` : 'rotate(0deg)',
       marginLeft: 'auto',
+      marginTop: 'auto',
     },
   })
 
   return (
-    <Grid item xs={12} sm={6} md={4} lg={3} style={styles.gridItem}>
+    <Grid item sm={12} md={6} lg={4} xl={3} style={styles.gridItem}>
       <Paper>
         <Card style={styles.card}>
           <CardHeader title={name} style={styles.cardHeader} />
           <CardContent>
-            <List>
-              <ListItem>
-                <ListItemText primary={`${newSnow}"`} secondary="New Snow" />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary={`${last48Hours}"`}
-                  secondary="48 Hours"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary={`${last7Days}"`} secondary="7 Days" />
-              </ListItem>
-            </List>
+            <Grid container justify="space-around">
+              <Grid item>
+                <List>
+                  <ListItem>
+                    <ListItemText
+                      primary={`${newSnow}"`}
+                      secondary="New Snow"
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary={`${last48Hours}"`}
+                      secondary="48 Hours"
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary={`${last7Days}"`}
+                      secondary="7 Days"
+                    />
+                  </ListItem>
+                </List>
+              </Grid>
+              <Grid item style={styles.weatherToday}>
+                <List>
+                  <ListItem>
+                    <ListItemText
+                      primary={weatherForecast[0].summaryDescription}
+                      secondary="Weather"
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary={`${weatherForecast[0].temperatureHigh} °F`}
+                      secondary="High"
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary={`${weatherForecast[0].temperatureLow} °F`}
+                      secondary="Low"
+                    />
+                  </ListItem>
+                </List>
+              </Grid>
+            </Grid>
           </CardContent>
-          <CardActions>
+          <CardActions style={{ bottom: 0 }}>
             <IconButton
               style={styles.expand}
               onClick={() => setExpanded(!expanded)}
@@ -101,10 +131,18 @@ const Resort: React.FC<{
           </CardActions>
           <Collapse in={expanded} timeout="auto" unmountOnExit>
             <CardContent>
-              <Typography variant="h6" paragraph>
-                {dayDescription}:
-              </Typography>
-              <Typography align="justify">{forecastString}</Typography>
+              <Grid container>
+                {weatherForecast.slice(1).map(dayForecast => (
+                  <Grid item key={dayForecast.daycode}>
+                    <Typography variant="h6">
+                      {dayForecast.dayDescription}
+                    </Typography>
+                    <Typography align="justify" paragraph>
+                      {dayForecast.forecastString}
+                    </Typography>
+                  </Grid>
+                ))}
+              </Grid>
             </CardContent>
           </Collapse>
         </Card>
